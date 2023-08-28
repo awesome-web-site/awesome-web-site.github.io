fffa5cd95e6cb6da106de714a5454992f4772f35
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-08-28 13:32:06 +0200

    uploaded minecraft world

---
+++ projects/minecraft-worlds/map-28.08.2023 [overviewer] (damaged, izometric)/overviewerConfig.js
@@ -0,0 +1,72 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "world"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1693202572",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Daytime Render",
+            "zoomLevels": 12,
+            "defaultZoom": 1,
+            "maxZoom": 12,
+            "path": "daytime",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world",
+            "last_rendertime": 1693031794,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                1882,
+                64,
+                -218
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                1882,
+                64,
+                -218
+            ],
+            "north_direction": 0
+        }
+    ]
+};
